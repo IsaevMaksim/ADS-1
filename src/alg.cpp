5dbe4fd46481bc27df085cf0ff234e05b2cf4a91
IsaevMaksim <78897852+IsaevMaksim@users.noreply.github.com>
2021-03-17 13:04:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,10 +4,10 @@ int cbinsearch(int *arr, int size, int value) {
 int i = 0, j = size - 1, count = 0, mid;
 while (i < j) {
 mid = (i + j) / 2;
-if (arr[mid] < value) {
+if (arr[mid] < value)
 i = mid + 1;
-}
-else j = mid;
+else
+j = mid;
 }
 if (arr[i] == value) {
 while (arr[i] == value) {
@@ -15,8 +15,8 @@ i++;
 count++;
 }
 }
-if (count > 0) {
+if (count > 0)
 return count;
-}
-else return 0;
+else
+return 0;
 }
