03db34f766fdb920cb707325342fc3a93b2dbefe
IsaevMaksim <78897852+IsaevMaksim@users.noreply.github.com>
2021-03-17 12:53:07 +0300

    Isaev

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int i = 0, int j = size - 1, count = 0, mid;
-  while (i < j) {
-		mid = (i + j) / 2;
-		if (arr[mid] < value) {
-			i = mid + 1;
-		}
-		else { j = mid; }
-	}
-	if (arr[i] == value) {
-		while (arr[i] == value) {
-			i++;
-			count++;
-		}
-	}
-	if (count > 0) {
-		return count;
-	}
-	else { return 0; }
+int i = 0, j = size - 1, count = 0, mid;
+while (i < j) {
+mid = (i + j) / 2;
+if (arr[mid] < value) {
+i = mid + 1;
+}
+else { j = mid; }
+}
+if (arr[i] == value) {
+while (arr[i] == value) {
+i++;
+count++;
+}
+}
+if (count > 0) {
+return count;
+}
+else { return 0; }
 }
